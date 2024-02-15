20c78c3f8be792cd1cb75b013c0858e76226b094
ozor <osortlan@gmail.com>
2024-02-15 22:11:21 +0100

    fix: IdempotencyKey changed to string

--- InvokePlayerActionRequest.cs
+++ InvokePlayerActionRequest.cs
@@ -2,7 +2,7 @@
 {
     public class InvokePlayerActionRequest
     {
-        public int IdempotencyKey { get; set; }
+        public string IdempotencyKey { get; set; }
         public string ActionName { get; set; } = string.Empty;
         public string Argument { get; set; } = string.Empty;
     }
